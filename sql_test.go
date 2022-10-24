d307d6cc4d4ece4607d07f0cb03eb762095c1020
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-24 12:55:27 +0700

    Query SQL

--- sql_test.go
+++ sql_test.go
@@ -14,10 +14,33 @@ func TestExecSql(t *testing.T) {
 
 	ctx := context.Background()
 
-	query := "INSERT INTO customer(id,name) VALUES('melza', 'Melza')"
-	_, err := db.ExecContext(ctx, query)
+	script := "INSERT INTO customer(id,name) VALUES('melza', 'Melza')"
+	_, err := db.ExecContext(ctx, script) //gunakan ExecContext apabila tidak memerlukan hasil
 	result := assert.Nil(t, err)
 	if result {
 		fmt.Println("Success Insert New Customer ")
 	}
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	fmt.Println("===========")
+	for rows.Next() {
+		var id, name string
+
+		err = rows.Scan(&id, &name)
+		fmt.Println("Id :", id)
+		fmt.Println("Name :", name)
+		fmt.Println("===========")
+	}
+
+	defer rows.Close()
+	assert.Nil(t, err)
 }
