613f9d071c60f5622a1812478070baacaac1eaab
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-24 22:49:29 +0700

    SQL Query Parameter

--- sql_test.go
+++ sql_test.go
@@ -112,3 +112,29 @@ func TestSqlInjection(t *testing.T) {
 
 	assert.Nil(t, err)
 }
+
+func TestSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "admin"
+
+	query := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, query, username, password)
+
+	if rows.Next() {
+		var username string
+		err = rows.Scan(&username)
+
+		fmt.Println("Login Berhasil :", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}
+
+	defer rows.Close()
+
+	assert.Nil(t, err)
+}
