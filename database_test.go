c10fd053fb00f029176c918bfe3a906a62bbc0e9
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-24 09:44:37 +0700

    Membuka Koneksi Ke Database

---
+++ database_test.go
@@ -0,0 +1,24 @@
+package gomysql
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpenConnection(t *testing.T) {
+	dbUsername := goDotEnvVariable("DB_USERNAME")
+	dbPassword := goDotEnvVariable("DB_PASSWORD")
+	dbHost := goDotEnvVariable("DB_HOST")
+	dbPort := goDotEnvVariable("DB_PORT")
+	dbName := goDotEnvVariable("DB_NAME")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("mysql", connectionString)
+	defer db.Close()
+	require.Nil(t, err, err)
+}
